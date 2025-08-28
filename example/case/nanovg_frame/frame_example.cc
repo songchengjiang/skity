ea2ca392b4e675c98bdacae3cffaa6f2fad58a7d
branjing <jinglezju@gmail.com>
2025-08-28 09:15:49 +0800

    fix(Win): Use math defines in math header of Windows

--- example/case/nanovg_frame/frame_example.cc
+++ example/case/nanovg_frame/frame_example.cc
@@ -2,6 +2,8 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 
+#define _USE_MATH_DEFINES
+
 #include <array>
 #include <cmath>
 #include <cstdlib>
